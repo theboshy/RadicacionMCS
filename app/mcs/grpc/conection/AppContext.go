73f3bf7d113080268e19d904f05c3dc1e30b4386
theboshy <opd011089>
2018-03-26 17:19:09 -0500

    fix ???

--- app/mcs/grpc/conection/AppContext.go
+++ app/mcs/grpc/conection/AppContext.go
@@ -14,10 +14,8 @@ const (
 	port = ":50051"
 )
 
-// server is used to implement helloworld.GreeterServer.
 type server struct{}
 
-// SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
 	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
 }
@@ -29,7 +27,6 @@ func main() {
 	}
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
-	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
